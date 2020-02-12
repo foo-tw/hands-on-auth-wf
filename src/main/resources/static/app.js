5915d4c5795ba3f45de25dd41cf6b8fec7c45250
Foo '0xf00' Meden <fmeden@thoughtworks.com>
2020-02-12 12:19:59 +0000

    added authentication

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -28,11 +28,33 @@ const AvailableTicket = ({destination, price, buyCallback}) => {
 };
 
 const App = () => {
+    const [user, setUser] = useState({});
+    const [accessToken, setAccessToken] = useState();
     const [availableTickets, setAvailableTickets] = useState([]);
     const [myTickets, setMyTickets] = useState([]);
     const buyCallback = (newTicket) => {
         setMyTickets(myTickets.concat([newTicket]));
     };
+    const redirectUri = "http://localhost:8080/"
+    useEffect(() => {
+        if(window.location.search.slice(1).split("=").indexOf("code") !== -1) {
+            const code = window.location.search.slice(1).split("code=")[1];
+            fetch('https://foo-tw.eu.auth0.com/oauth/token', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded'
+                },
+                body: `audience=teleport-tickets-techsession-api&client_id=k9BRqtBF9q8tWHqWpf96GAeF58t3cMcq&grant_type=authorization_code&code=${code}&code_verifier=9999&redirect_uri=${redirectUri}`,
+            }).then(r => r.json())
+            .then(r => {
+                // TODO at this point you should verify checksums and sigs of the id_token
+                const payload = JSON.parse(atob(r.id_token.split('.')[1]));
+                setUser(payload);
+                setAccessToken(r.access_token);
+            });
+        }
+    },[]);
     useEffect(() => {
         Promise.all([
             fetch('/api/v0/available-tickets')
@@ -45,18 +67,26 @@ const App = () => {
                 setMyTickets(myTickets);
             });
     }, []);
-    return (
+    if(!user.nickname) {
+        return (
             <div>
-                <h1>Hi Foo</h1>
-                <h2>Here are the tickets you can buy</h2>
-                <ul>
-                    {availableTickets.map((ticket, index) => (<AvailableTicket key={index} destination={ticket.destination} price={ticket.price} buyCallback={buyCallback}/>))}
-                </ul>
-                <h2>Here are your tickets</h2>
-                <ul>
-                    {myTickets.map((ticket, index) => (<BoughtTicket key={index} destination={ticket.destination} price={ticket.price} />))}
-                </ul>
+                <h1>Hi Unknown</h1>
+                <p>Please <a href="https://foo-tw.eu.auth0.com/authorize?response_type=code&client_id=k9BRqtBF9q8tWHqWpf96GAeF58t3cMcq&redirect_uri=http://localhost:8080&code_challenge=9999&audience=teleport-tickets-techsession-api&scope=openid%20profile">log in</a></p>
             </div>);
+    
+    }
+    return (
+        <div>
+            <h1>Hi {user.nickname}</h1>
+            <h2>Here are the tickets you can buy</h2>
+            <ul>
+                {availableTickets.map((ticket, index) => (<AvailableTicket key={index} destination={ticket.destination} price={ticket.price} buyCallback={buyCallback}/>))}
+            </ul>
+            <h2>Here are your tickets</h2>
+            <ul>
+                {myTickets.map((ticket, index) => (<BoughtTicket key={index} destination={ticket.destination} price={ticket.price} />))}
+            </ul>
+        </div>);
 }
 
 const domContainer = document.querySelector('div.content');
